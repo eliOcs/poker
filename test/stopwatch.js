f85662790d565972aaa49e7dcfc636d334f3abdf
Elio Capella <elio.capella@filestage.io>
2020-04-24 19:50:20 +0200

    add stop watch domain logic

---
+++ test/stopwatch.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const tap = require("tap");
+const sinon = require("sinon");
+const Stopwatch = require("../src/stopwatch");
+const stopwatch = Stopwatch();
+const clock = sinon.useFakeTimers();
+
+tap.same(stopwatch.toJSON(), {
+  time: "00:00:00",
+  running: false,
+});
+stopwatch.start();
+
+clock.tick(1000 /* 1 second */);
+tap.same(stopwatch.toJSON(), {
+  time: "00:00:01",
+  running: true,
+});
+
+clock.tick(60000 /* 1 minute */);
+tap.same(stopwatch.toJSON(), {
+  time: "00:01:01",
+  running: true,
+});
+
+clock.tick(3600000 /* 1 hour */);
+tap.same(stopwatch.toJSON(), {
+  time: "01:01:01",
+  running: true,
+});
+
+stopwatch.stop();
+tap.same(stopwatch.toJSON(), {
+  time: "01:01:01",
+  running: false,
+});
+
+stopwatch.reset();
+tap.same(stopwatch.toJSON(), {
+  time: "00:00:00",
+  running: false,
+});
