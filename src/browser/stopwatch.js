d8790f413bc95d0659bc0754a3c2bd2ecf742d4f
Elio Capella <elio.capella@filestage.io>
2020-04-25 22:20:37 +0200

    browser UI compoment finished

--- src/browser/stopwatch.js
+++ src/browser/stopwatch.js
@@ -3,18 +3,38 @@ import { LitElement, html } from "lit-element";
 export class Stopwatch extends LitElement {
   constructor() {
     super();
-    this.data = {
-      time: "00:00:00",
-      running: false,
+    this.websocket = new WebSocket("url");
+    this.websocket.onmessage = (event) => {
+      this.data = JSON.parse(event.data);
     };
   }
 
+  static get properties() {
+    return { data: { type: Object } };
+  }
+
+  start() {
+    this.websocket.send("start");
+  }
+
+  stop() {
+    this.websocket.send("stop");
+  }
+
+  reset() {
+    this.websocket.send("reset");
+  }
+
   render() {
-    return html`
-      <span>${this.data.time}</span>
-      <button>${this.data.running ? "Stop" : "Start"}</button>
-      <button>Reset</button>
-    `;
+    if (!this.data) {
+      return html`<span>Loading ...</span>`;
+    }
+
+    return html` <span data-test="time">${this.data.time}</span>
+      ${this.data.running
+        ? html`<button data-test="stop" @click=${this.stop}>Stop</button>`
+        : html`<button data-test="start" @click=${this.start}>Start</button>`}
+      <button data-test="reset" @click=${this.reset}>Reset</button>`;
   }
 }
 
