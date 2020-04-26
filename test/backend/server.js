39e15db2906ae85d160cce069b250eab9df22f11
Elio Capella <elio.capella@filestage.io>
2020-04-26 10:17:44 +0200

    add tests for backend code

--- test/backend/server.js
+++ test/backend/server.js
@@ -1,8 +1,36 @@
 "use strict";
 
+const tap = require("tap");
 const sinon = require("sinon");
+const WebSocket = require("ws");
+const Server = require("../../src/backend/server");
 
 const stopwatch = {
-  toJSON: sinon.stub(),
+  start: sinon.stub(),
+  stop: sinon.stub(),
+  reset: sinon.stub(),
+  toJSON: sinon.stub().returns({ json: true }),
 };
-const server = Server({ stopwatch });
+const webSocket = { on: sinon.stub(), send: sinon.stub() };
+const webSocketServer = { on: sinon.stub() };
+sinon.stub(WebSocket, "Server").returns(webSocketServer);
+
+Server({ stopwatch });
+tap.same(WebSocket.Server.firstCall.args, [{ port: 8080 }]);
+
+webSocketServer.on.withArgs("connection").callArgWith(1, webSocket);
+tap.same(webSocket.send.firstCall.args, ['{"json":true}']);
+
+stopwatch.onUpdate(stopwatch);
+tap.same(webSocket.send.secondCall.args, ['{"json":true}']);
+
+webSocket.on.withArgs("message").callArgWith(1, "start");
+tap.true(stopwatch.start.called);
+
+webSocket.on.withArgs("message").callArgWith(1, "stop");
+tap.true(stopwatch.stop.called);
+
+webSocket.on.withArgs("message").callArgWith(1, "reset");
+tap.true(stopwatch.reset.called);
+
+WebSocket.Server.restore();
