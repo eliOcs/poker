67c86699630db15457ec2e61afab8e52ca4fe803
Elio Capella <elio.capella@filestage.io>
2021-09-21 21:47:14 +0200

    it is tricky to reuse http2 server for websockets, added websocket server and migrated to https

--- src/backend.js
+++ src/backend.js
@@ -1,14 +1,12 @@
-import * as fs from "fs/promises";
-import { readFileSync } from "fs";
+import * as fs from "fs";
 import path from "path";
 import mime from "mime-types";
-import http2 from "http2";
-const { HTTP2_HEADER_METHOD, HTTP2_HEADER_PATH, HTTP2_HEADER_STATUS } =
-  http2.constants;
+import https from "https";
+import { WebSocketServer } from "ws";
 
-const server = http2.createSecureServer({
-  key: readFileSync(process.env.HTTPS_KEY),
-  cert: readFileSync(process.env.HTTPS_CERT),
+const server = https.createServer({
+  key: fs.readFileSync(process.env.HTTPS_KEY),
+  cert: fs.readFileSync(process.env.HTTPS_CERT),
 });
 
 server.on("error", (err) => console.error(err));
@@ -18,27 +16,29 @@ const files = {
   "/index.js": "src/frontend/index.js",
 };
 
-async function respondWithFile(filePath, stream) {
-  const fileDescriptor = await fs.open(filePath, "r");
-  const stat = await fileDescriptor.stat();
-  const headers = {
-    "content-length": stat.size,
-    "last-modified": stat.mtime.toUTCString(),
+function respondWithFile(filePath, res) {
+  res.writeHead(200, {
     "content-type": mime.contentType(path.extname(filePath)),
-  };
-  stream.respondWithFD(fileDescriptor, headers);
-  stream.on("close", async () => await fileDescriptor.close());
+  });
+  fs.createReadStream(filePath).pipe(res);
 }
 
-server.on("stream", async (stream, headers) => {
-  const method = headers[HTTP2_HEADER_METHOD];
-  const path = headers[HTTP2_HEADER_PATH];
-  if (method === "GET" && path in files) {
-    await respondWithFile(files[path], stream);
+server.on("request", (req, res) => {
+  if (req.method === "GET" && req.url in files) {
+    respondWithFile(files[req.url], res);
   } else {
-    stream.respond({ [HTTP2_HEADER_STATUS]: 404 });
-    stream.end();
+    res.writeHead(404);
+    res.end();
   }
+});
+
+const wss = new WebSocketServer({ server });
+wss.on("connection", function connection(ws) {
+  ws.on("message", function incoming(message) {
+    console.log("received: %s", message);
+  });
+
+  ws.send("something");
 });
 
 server.listen(process.env.PORT);
--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+const socket = new WebSocket("wss://localhost:8443");
+
+socket.onopen = function () {
+  console.log("[open] Connection established");
+};
+
+socket.onmessage = function (event) {
+  console.log(`[message] Data received from server: ${event.data}`);
+};
