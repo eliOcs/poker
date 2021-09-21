48d09743c277c62524b8973be60ad6797d25b381
Elio Capella <elio.capella@filestage.io>
2021-09-21 19:29:21 +0200

    use async fs

--- src/backend.js
+++ src/backend.js
@@ -1,4 +1,5 @@
-import fs from "fs";
+import * as fs from "fs/promises";
+import { readFileSync } from "fs";
 import path from "path";
 import mime from "mime-types";
 import http2 from "http2";
@@ -6,8 +7,8 @@ const { HTTP2_HEADER_METHOD, HTTP2_HEADER_PATH, HTTP2_HEADER_STATUS } =
   http2.constants;
 
 const server = http2.createSecureServer({
-  key: fs.readFileSync(process.env.HTTPS_KEY),
-  cert: fs.readFileSync(process.env.HTTPS_CERT),
+  key: readFileSync(process.env.HTTPS_KEY),
+  cert: readFileSync(process.env.HTTPS_CERT),
 });
 
 server.on("error", (err) => console.error(err));
@@ -17,23 +18,23 @@ const files = {
   "/index.js": "src/frontend/index.js",
 };
 
-function respondWithFile(filePath, stream) {
-  const fileDescriptor = fs.openSync(filePath, "r");
-  const stat = fs.fstatSync(fileDescriptor);
+async function respondWithFile(filePath, stream) {
+  const fileDescriptor = await fs.open(filePath, "r");
+  const stat = await fileDescriptor.stat();
   const headers = {
     "content-length": stat.size,
     "last-modified": stat.mtime.toUTCString(),
     "content-type": mime.contentType(path.extname(filePath)),
   };
   stream.respondWithFD(fileDescriptor, headers);
-  stream.on("close", () => fs.closeSync(fileDescriptor));
+  stream.on("close", async () => await fileDescriptor.close());
 }
 
-server.on("stream", (stream, headers) => {
+server.on("stream", async (stream, headers) => {
   const method = headers[HTTP2_HEADER_METHOD];
   const path = headers[HTTP2_HEADER_PATH];
   if (method === "GET" && path in files) {
-    respondWithFile(files[path], stream);
+    await respondWithFile(files[path], stream);
   } else {
     stream.respond({ [HTTP2_HEADER_STATUS]: 404 });
     stream.end();
