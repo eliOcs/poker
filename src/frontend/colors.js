09e0d972baa593de5bfc76b98f32957433a4b871
Elio Capella <elio.capella@filestage.io>
2021-10-02 10:06:32 +0200

    use web components

---
+++ src/frontend/colors.js
@@ -0,0 +1,16 @@
+export const base00 = "#181818"; // Default Background
+export const base01 = "#282828"; // Lighter Background (Used for status bars, line number and folding marks)
+export const base02 = "#383838"; // Selection Background
+export const base03 = "#585858"; // Comments, Invisibles, Line Highlighting
+export const base04 = "#b8b8b8"; // Dark Foreground (Used for status bars)
+export const base05 = "#d8d8d8"; // Default Foreground, Caret, Delimiters, Operators
+export const base06 = "#e8e8e8"; // Light Foreground (Not often used)
+export const base07 = "#f8f8f8"; // Light Background (Not often used)
+export const base08 = "#ab4642"; // Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
+export const base09 = "#dc9656"; // Integers, Boolean, Constants, XML Attributes, Markup Link Url
+export const base0A = "#f7ca88"; // Classes, Markup Bold, Search Text Background
+export const base0B = "#a1b56c"; // Strings, Inherited Class, Markup Code, Diff Inserted
+export const base0C = "#86c1b9"; // Support, Regular Expressions, Escape Characters, Markup Quotes
+export const base0D = "#7cafc2"; // Functions, Methods, Attribute IDs, Headings
+export const base0E = "#ba8baf"; // Keywords, Storage, Selector, Markup Italic, Diff Changed
+export const base0F = "#a16946"; // Deprecated, Opening/Closing Embedded Language Tags
