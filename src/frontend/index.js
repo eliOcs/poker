1e0471fd3636314bd6e67f0a9ca4104273a4f320
Elio Capella <me@eliocapella.com>
2026-01-01 19:14:56 +0100

    Increase text and icon size in hole cards

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -228,6 +228,14 @@ class Game extends LitElement {
         margin-top: auto;
       }
 
+      .hole-cards .card .rank {
+        font-size: 1.2em;
+      }
+
+      .hole-cards .card .suit {
+        font-size: 1.3em;
+      }
+
       .card {
         display: inline-flex;
         flex-direction: column;
